46d50c4fa1065d0f3a2cab6f71d339ba4ff8e404
bradwang <bradwang815@outlook.com>
2025-07-02 12:39:08 -0600

    zombie struct created

--- Contract.sol
+++ Contract.sol
@@ -3,4 +3,11 @@ pragma solidity ^0.8.30;
 contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
 }
