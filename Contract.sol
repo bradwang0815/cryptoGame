3f93c3c517b292a9256be455861c27cd2afd45a2
bradwang <bradwang815@outlook.com>
2025-07-02 13:30:55 -0600

    New Zombie event created

--- Contract.sol
+++ Contract.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.8.30;
 
 contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -12,11 +15,18 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function createZombie(string memory _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodedPacked(_str)));
         return rand % dnaModulus;
     }
+
+    function createRandomeZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
 }
